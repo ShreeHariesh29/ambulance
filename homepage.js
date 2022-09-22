80065526bddf34aaf8e37dedf35a1bc973fcc8ec
Shree haresh <shreehariesh29@gmail.com>
2022-09-22 20:28:08 +0530

    just

--- homepage.js
+++ homepage.js
@@ -1,3 +1,13 @@
-if ( first== "shreehariesh"){
-    
+function pwdverify()
+{
+    uname=document.getElementById("first").value;
+    pwd=document.getElementById("second").value;
+    if(uname=="admin" && pwd=="123")
+    {
+        
+        window.open("nextpage.html");
+    }
+    else {
+        window.alert("Incorrect email.id and password")
+    }
 }
